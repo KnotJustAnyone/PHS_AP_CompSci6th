a9a40b31a6e9a2221d7bf48a91d1fc2c176d649e
DrChapman <kyle.chapman.topology@gmail.com>
2025-10-14 13:58:46 -0700

    Add files via upload

---
+++ sudoku (1).py
@@ -0,0 +1,12 @@
+import numpy
+import random
+class sudoku_board:
+    def __init__(self):
+        sudoku_boards={"easy":[],"medium":[],"hard":[]}
+    def guess(self,x,y,z):
+        pass
+    def check(self,x,y):
+        pass
+    def congrats(self):
+        #if everything is right, good job, pat on the back
+        pass
