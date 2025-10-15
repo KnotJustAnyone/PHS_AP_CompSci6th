30b7febea6005c62dce916ca7445105d082506ba
Kayla <142340496+ScorpiusSuperior@users.noreply.github.com>
2025-10-15 14:25:58 -0700

    Update sudoku.py
    Guess function

--- sudoku.py
+++ sudoku.py
@@ -3,8 +3,15 @@ import random
 class sudoku_board:
     def __init__(self):
         sudoku_boards={"easy":[],"medium":[],"hard":[]}
-    def guess(self,x,y,z):
-        pass
+        self.board=numpy.full(81,0).reshape(9,9)
+    def guess(self):
+        print("What is your guess?")
+        z=int(input())
+        print("Where do you want to put it? (x-coordinate, 1-9)")
+        x=int(input())-1
+        print("Where do you want to put it? (y-coordinate, 1-9)")
+        y=9-int(input())
+        self.board[y][x]=z
     def check(self,x,y):
         pass
     def congrats(self):
