2aa29fb3f6707013841704e0d3ea1e6dd5edbc56
Half2beC00L <166662851+Half2beC00L@users.noreply.github.com>
2025-11-12 18:42:20 -0800

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,64 @@
-class Player():
+import random
+players = [] 
 
-class Dealer():
+def getting_players():
+    pass
+  
+def card_value(card):
+    pass
 
-#tests-------------------------------------------------------------
+class Player:
+    def __init__(self): 
+        pass
+      
+    def newcard(self): 
+        pass
+      
+    def resethand(self):
+        pass
+      
+    def handsum(self):
+        pass
+      
+    def getbet(self): 
+        pass
+      
+    def splitting(self):
+        pass
+      
+    def doubledown(self):
+        pass
+      
+    def insurance(self):
+        pass
+      
+    
+class Dealer: 
+    def __init__(self, players): 
+        pass
+
+    def deal1(self):
+        pass
+      
+    def playerbets(self): 
+        pass 
+    
+    def dealershow(self): 
+        pass
+    
+    def round(self):
+        pass
+      
+    def dealerturn(self): 
+        pass
+
+    def dealer_value(self): 
+        pass
+
+    def check(self): 
+        pass
+
+#Tests: -------------------------------------------------------------------------------------
+
+
+
