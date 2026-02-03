7f3142f291f98ce7b0a997193468557e6a494156
Giselle700 <giselleleyva782@gmail.com>
2026-02-02 20:28:56 -0800

    Implement player turns and input handling
    Refactor game loop to alternate players and handle input.

--- connect_4.py
+++ connect_4.py
@@ -19,16 +19,26 @@ def print_board(board):
     print("  1 2 3 4 5 6 7")
     #sets up display board, column numbers are listed underneath
 
-column = int(input("Choose a column (1-7):"))-1
-#asks user to choose a column to drop the play piece
-
 def play_piece(board, column, symbol):
     for row in range(ROWS-1,-1,-1):
         if board[row][column] == ".":
             board[row][column] = symbol
             return
     #plays the piece by dropping it the selected column
-        
-play_piece(board, column, "X")
-print_board(board)
 
+#Player X is player 1, Player O is player 2
+current_player = "X"
+while True:
+    print_board(board)
+    print("Player", current_player, "turn")
+    #asks user to choose a column to drop the play piece
+    column = int(input("Choose a column (1-7):"))-1
+    play_piece(board, column, current_player)
+
+    #switch players
+    if current_player == "X":
+        current_player = "O"
+    else:
+        current_player = "X"
+
+print_board(board)
