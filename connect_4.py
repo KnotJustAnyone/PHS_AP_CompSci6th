3845d9820d9c805cc2e6583866b9923090090a25
Giselle700 <giselleleyva782@gmail.com>
2026-02-10 14:08:57 -0800

    added check for full column and check winner
    code now checks if the player's selected column is full and if a player has won

--- connect_4.py
+++ connect_4.py
@@ -19,26 +19,67 @@ def print_board(board):
     print("  1 2 3 4 5 6 7")
     #sets up display board, column numbers are listed underneath
 
-def play_piece(board, column, symbol):
+def play_piece(board, column, current_player):
     for row in range(ROWS-1,-1,-1):
         if board[row][column] == ".":
-            board[row][column] = symbol
+            board[row][column] = current_player
             return
     #plays the piece by dropping it the selected column
+def valid_move(board,column):
+    return board[0][column]=="."
+    #checks if the selected column is full
+
+def check_winner(board, current_player):
+    #Horizontal 
+    for r in range(ROWS):
+        for c in range(COLUMNS-3):
+            if board[r][c]==current_player and board[r][c+1]==current_player and board[r][c+2]==current_player and board[r][c+3]==current_player:
+                return True
+    #Vertical
+    for r in range(ROWS-3):
+        for c in range(COLUMNS):
+            if board[r][c]==current_player and board[r+1][c]==current_player and board[r+2][c]==current_player and board[r+3][c]==current_player:
+                return True
+    #Diagonal down-right
+    for r in range(ROWS-3):
+        for c in range(COLUMNS-3):
+            if board[r][c]==current_player and board[r+1][c+1]==current_player and board[r+2][c+2]==current_player and board[r+3][c+3]==current_player:
+                return True
+    #Diagonal up-right
+    for r in range(3,ROWS):
+        for c in range(COLUMNS-3):
+            if board[r][c]==current_player and board[r-1][c+1]==current_player and board[r-2][c+2]==current_player and board[r-3][c+3]==current_player:
+                return True
+            
+    return False
+
 
 #Player X is player 1, Player O is player 2
 current_player = "X"
+
 while True:
     print_board(board)
     print("Player", current_player, "turn")
-    #asks user to choose a column to drop the play piece
+
     column = int(input("Choose a column (1-7):"))-1
+    #asks user to choose a column to drop the play piece
+    if column <0 or column >= COLUMNS:
+        print("Invalid column. Try again. :]")
+        continue
+    if not valid_move(board, column):
+        print("That column is full. Try again :/")
+        continue
+
     play_piece(board, column, current_player)
+
+    #Check for winner
+    if check_winner(board, current_player):
+        print_board(board)
+        print("Player", current_player, "wins! :D")
+        break
 
     #switch players
     if current_player == "X":
         current_player = "O"
     else:
         current_player = "X"
-
-print_board(board)
