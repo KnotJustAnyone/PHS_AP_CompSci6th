ae91b420e84c686c48be86aa8b437bbc81c039f4
DrChapman <kyle.chapman.topology@gmail.com>
2026-03-27 14:01:55 -0700

    Update connect_4_training.py

--- connect_4_training.py
+++ connect_4_training.py
@@ -5,6 +5,33 @@ COLUMNS = 7
 
 import connect_4 as c_4
 
+def play_game():
+    print("Who will be playing as X?")
+    print("1) Human")
+    print("Any other key to return to main menu")
+    choice = input()
+    if choice == '1':
+        x_strat = human_input
+    else:
+        return None
+    print("Who will be playing as y?")
+    print("1) Human")
+    print("Any other key to return to main menu")
+    choice = input()
+    if choice == '1':
+        o_strat = human_input
+    print("Begining Game...")
+    board = c_4.create_board()
+    current_player = 'X'
+    game_on = True
+    while game_on:
+        game_on = False #Development paused here
+    return None
+
+def human_input(board):
+    print("Human Strategy not Ready")
+    return None
+
 def main_menu():
     choice = None
     while choice != 2:
@@ -13,7 +40,7 @@ def main_menu():
         print("2) Quit")
         choice = input()
         if choice == "1":
-            print("Still In Development, exiting")
+            play_game()
         elif choice == "2":
             print("Thanks for playing")
             exit()
