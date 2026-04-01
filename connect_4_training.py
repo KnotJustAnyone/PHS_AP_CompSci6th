0dddc26bda4f9fd8df7a550b3ef5fe5cc009cbbb
DrChapman <kyle.chapman.topology@gmail.com>
2026-04-01 14:09:21 -0700

    Finish Human connect_4

--- connect_4_training.py
+++ connect_4_training.py
@@ -25,21 +25,40 @@ def play_game():
     board = c_4.create_board()
     current_player = 'X'
     game_on = True
-    while game_on:
-        column = strats[current_player](board)
+    attempts = 0
+    while game_on and attempts < 10:
+        column = strats[current_player](board,current_player)
         if type(column) != type(0):
             print(f"{current_player} did not give a column, trying again...")
-        if column < 0 or column >= COLUMNS:
+            attempts += 1
+        elif column < 0 or column >= COLUMNS:
             print(f"{current_player} attempted to play off board, trying again...")
-            continue
-        if not c_4.valid_move(board, column):
+            attempts += 1
+        elif not c_4.valid_move(board, column):
             print(f"{current_player} attempted to play in a full column, trying again...")
-            continue
+            attempts += 1
+        else:
+            c_4.play_piece(board, column, current_player)
+            if c_4.check_winner(board, current_player):
+                c_4.print_board(board)
+                print("Player", current_player, "wins! :D")
+                game_on = False
+            elif c_4.board_full(board):
+                c_4.print_board(board)
+                print("It's a tie! :O")
+                game_on = False
+            attempts = 0
+            if current_player == 'X':
+                current_player = 'O'
+            else:
+                current_player = 'X'
     return None
 
-def human_input(board):
-    print("Human Strategy not Ready")
-    return None
+def human_input(board, current_player):
+    c_4.print_board(board)
+    print(f"Player {current_player}, which column do you want to play in?")
+    column = int(input("Choose a column (1-7):"))-1
+    return column
 
 def main_menu():
     choice = None
